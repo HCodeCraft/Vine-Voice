604d51701dfc51e71559eac9c2da9b0d3f5dace3
Hanna <marmalademarmoset@gmail.com>
2023-09-15 13:32:02 -0700

    Add rescue, before_action:authorize, find_user, authorize and render_unprossessable_entitiy_response to ApplicationController

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!([{
-  username: "HarvestDreamer",
-  name: "Hanna"
-  password: "Kcc9g5f_Dc3h%-1K",
-  avatar_url: "https://i.pinimg.com/originals/c8/6f/8a/c86f8a7ecda86bf1021e56334557d4cc.jpg",
-  image: "",
-  privacy: false,
-  email:"hcodecraft@gmail.com",
-  recieve_dev_emails: true,
-  status: "Coding like a boss 💚 💪",
-  admin: true
-
-}])
