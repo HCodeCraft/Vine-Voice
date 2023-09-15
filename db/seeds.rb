7546ff2e2dfefe7e4ad3788ab3b42f5b9281b424
Hanna <marmalademarmoset@gmail.com>
2023-09-15 13:17:51 -0700

    Add deployed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!([{
+  username: "OrchardDreamer",
+  name: "Hanna"
+  password: "Kcc9g5f_Dc3h%-1K",
+  avatar_url: "",
+  image: "",
+  privacy: false,
+  email:"hcodecraft@gmail.com",
+  recieve_dev_emails: true,
+  status: "Coding like a boss 💚 💪",
+  admin: true
+
+}])
