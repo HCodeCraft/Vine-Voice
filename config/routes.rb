cfa0a2cc5dec66c03f3b868017e17e15a1823240
Hanna <marmalademarmoset@gmail.com>
2023-09-16 16:41:05 -0700

    Remove namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  
-  namespace :api do
-    resources :plants, only: [:index]
-    post "/signup", to: "users#create"
-    get "/me", to: "users#show"
-    post "/login", to: "sessions#create"
-    delete "/logout", to: "sessions#destroy"
-  end
+  # Routes for plants, users, sessions, etc.
+  resources :plants, only: [:index]
+  resources :users, only: [:create, :show, :index]
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
+  # Fallback route for client-side routing (e.g., React, Vue.js)
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
+
+
 
 
   # For more details on this file's DSL, see https://guides.rubyonrails.org/routing.html
