1da6d686f1dfffb6e4083d520e5e5408d44385a7
Hanna <marmalademarmoset@gmail.com>
2023-10-16 14:16:54 -0700

    Filled out EditProfile component to have user state in fields

--- client/src/features/users/EditProfile.js
+++ client/src/features/users/EditProfile.js
@@ -1,36 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import {
   Container,
   Typography,
   Button,
   FormGroup,
   FormControlLabel,
-  Checkbox, Box
-} from "@mui/material";
-import TextField from "@mui/material/TextField";
-import CommonButton from "../../common/CommonButton";
+  Checkbox,
+  Box,
+} from '@mui/material';
+import TextField from '@mui/material/TextField';
+import CommonButton from '../../common/CommonButton';
+import { useSelector } from 'react-redux';
 
 const EditProfile = () => {
+  const currentUser = useSelector((state) => state.user.loggedInUser);
 
   const [user, setUser] = useState({
-    username:"",
-    name: "",
-    image:"",
-    email:"",
+    username: '',
+    name: '',
+    image: '',
+    email: '',
     receive_dev_emails: false,
-    status:""
-  })
+    status: '',
+  });
+
+  useEffect(() => {
+    if (currentUser) {
+      setUser(currentUser);
+    }
+  }, [currentUser]);
 
   const boxStyle = {
-    backgroundColor: "#f5f5f5", 
-    padding: "20px",
-  }
-
-
+    backgroundColor: '#f5f5f5',
+    padding: '30px',
+    borderRadius: '10px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+  };
 
   const handleUserChange = (e) => {
-    const value =
-      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
     setUser({
       ...user,
       [e.target.name]: value,
@@ -38,88 +46,90 @@ const EditProfile = () => {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-
-  }
-
+    e.preventDefault();
+    // Handle form submission here
+  };
 
   return (
-<>
-  <br />
-  <br />
-  <Box sx={boxStyle}>
-    <Typography variant="h5">Edit My User Details</Typography>
-    <br />
-    <br/>
-      <br/>
+    <>
+      <br />
+      <br />
+      <Container maxWidth="md">
+        <Box sx={boxStyle}>
+          <Typography variant="h5">Edit My User Details</Typography>
+          <br/>
+          <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+            <Box mb={2}> {/* Add margin bottom */}
+              <Button variant="contained" component="label" color="primary">
+                Change Profile Picture
+                <input type="file" hidden />
+              </Button>
+            </Box>
+            <br/>
+            <Box mb={2}> {/* Add margin bottom */}
+              <TextField
+                label="Username"
+                name="username"
+                variant="outlined"
+                color="secondary"
+                fullWidth
+                value={user.username}
+                onChange={handleUserChange}
+              />
+            </Box>
+            <Box mb={2}> {/* Add margin bottom */}
+              <TextField
+                label="Name"
+                name="name"
+                variant="outlined"
+                color="secondary"
+                fullWidth
+                value={user.name}
+                onChange={handleUserChange}
+              />
+            </Box>
+            <Box mb={2}> {/* Add margin bottom */}
+              <TextField
+                label="Status"
+                name="status"
+                variant="outlined"
+                color="secondary"
+                multiline
+                rows={4}
+                fullWidth
+                value={user.status}
+                onChange={handleUserChange}
+              />
+            </Box>
+            <Box mb={2}> {/* Add margin bottom */}
+              <TextField
+                label="Email"
+                name="email"
+                variant="outlined"
+                color="secondary"
+                fullWidth
+                value={user.email}
+                onChange={handleUserChange}
+              />
+            </Box>
+            <Box mb={2}> {/* Add margin bottom */}
+              <FormGroup>
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label="Receive Dev Emails"
+                  name="receive_dev_emails"
+                  onChange={handleUserChange}
+                />
+              </FormGroup>
+            </Box>
+            <CommonButton onClick={handleSubmit}>Submit</CommonButton>
+          </form>
+        </Box>
+      </Container>
+    </>
+  );
+};
 
-    <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-    <Button variant="contained" component="label" color="primary">
-        {" "}
-        Change Profile Picture
-        <input type="file" hidden />
-        {/* need to add an onchange to this */}
-      </Button>
-      <br/>
-      <br/>
-  
-      <TextField
-        label="Username"
-        name="username"
-        variant="outlined"
-        color="secondary"
-        className="classes-field"
-        onChange={handleUserChange}
-      />
-      <br />
-      <br />
-      <TextField
-        label="Name"
-        name="name"
-        variant="outlined"
-        color="secondary"
-        className="classes-field"
-        onChange={handleUserChange}
-      />
-      <br />
-      <br />
-      <TextField
-        label="Status"
-        name="status"
-        variant="outlined"
-        color="secondary"
-        className="classes-field"
-        multiline
-        rows={2}
-        onChange={handleUserChange}
-      />
-      <br />
-      <br />
-      <TextField
-        label="Email"
-        name="email"
-        variant="outlined"
-        color="secondary"
-        className="classes-field"
-        onChange={handleUserChange}
-      />
-      <br />
-      <br />
-      <FormGroup>
-        <FormControlLabel
-          control={<Checkbox />}
-          label="Receive Dev Emails"
-          name="receive_dev_emails"
-          onChange={handleUserChange}
-        />
-      </FormGroup>
-      <br />
-      <CommonButton onClick={handleSubmit}>Submit</CommonButton>
-    </form>
-  </Box>
-</>
+export default EditProfile;
 
-  )
-}
 
-export default EditProfile
