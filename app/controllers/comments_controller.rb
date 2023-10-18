9d8076831234d6e39015fe3c388636c753d2daa5
Hanna <marmalademarmoset@gmail.com>
2023-10-18 10:32:16 -0700

    Fill out comments_controller with create, update, find_comment, and comment_params methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+
+    # create the comment though the user
+    def create
+        newComment = @current_user.comment.create!(comment_params)
+        render json: newComment
+    end
+
+    def update
+        comment = find_comment
+        comment.update(comment_params)
+        render json: comment
+    end
+
+    private
+
+    def find_comment
+        comment = @current_user.comments.find_by(id: params[:id])
+      end
+    
+
+      # not sure if I need to include user_id?
+      def comment_params
+        params.require(:comment).permit(:text, :entry_id)
+      end
 end
