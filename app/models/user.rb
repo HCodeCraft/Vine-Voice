f8ebc8386cffe16a54230dd4eb9c7334a7affe9f
Hanna <marmalademarmoset@gmail.com>
2024-01-05 13:01:05 -0800

    Make usernames case insensitive when claiming

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,21 @@ class User < ApplicationRecord
   has_many :plants, -> { distinct }, through: :entries
   has_secure_password
   has_one_attached :avatar
+  before_validation :normalize_username
 
-  validates :username, presence: true, length: { in: 3..15 }, uniqueness: true, format: { with: /\A[^-\s_]+\z/, message: "must not contain spaces, dashes, or underscores" }, unless: :username_not_changed?
+ validates :username, presence: true, length: { in: 3..15 }, uniqueness: { case_sensitive: false }, format: { with: /\A[^-\s_]+\z/, message: "must not contain spaces, dashes, or underscores" }, unless: :username_not_changed?
   validates :email, presence: true, uniqueness: { on: :create }
   validates :password, presence: true, length: { minimum: 5 }, on: :create
+
+
 
   def username_not_changed?
     username_was == username
   end
+
+  def normalize_username
+  self.username = username.downcase
+end
 
   def avatar_url
     Rails.application.routes.url_helpers.url_for(avatar) if avatar.attached?
