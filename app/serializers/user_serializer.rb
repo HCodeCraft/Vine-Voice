65d9619b72ec324f03e5ccf70255f6eaddd60c39
Hanna <marmalademarmoset@gmail.com>
2024-01-05 16:13:04 -0800

    Test a different avatar_thumbnail method

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -8,9 +8,22 @@ class UserSerializer < ActiveModel::Serializer
     rails_blob_path(object.avatar, only_path: true) if object.avatar.attached?
   end
 
+  # def avatar_thumbnail
+  #   rails_representation_url(object.avatar.variant(resize_to_limit: [200, 200]).processed, only_path: true) if object.avatar.attached?
+  # end
+
   def avatar_thumbnail
-    rails_representation_url(object.avatar.variant(resize_to_limit: [200, 200]).processed, only_path: true) if object.avatar.attached?
+  if object.avatar.attached?
+    begin
+      variant = object.avatar.variant(resize_to_limit: [200, 200]).processed
+      rails_representation_url(variant, only_path: true)
+    rescue ActiveStorage::FileNotFoundError
+      # Handle the case where the file is not found
+      # For example, you can provide a default avatar URL or return nil
+      nil
+    end
   end
+end
 
 
 end
